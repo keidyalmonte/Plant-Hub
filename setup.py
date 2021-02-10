f89e4ef513468dfa0bd1680b4e479396f2fc4249
Keidy Almonte <ka1783@nyu.edu>
2021-02-10 13:47:05 -0500

    Added stub software library, see #2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='planthub',
+      version='1.0',
+      description='CompE Stub Library',
+      author='Keidy Almonte',
+      author_email='ka1783@nyu.edu',
+      url='https://github.com/keidyalmonte/Plant-Hub',
+      py_modules=['planthub'],
+     )
+
