c2a11b84fefe48c4dbf8268a2a66d78c6101f3f6
Keidy Almonte <ka1783@nyu.edu>
2021-02-24 10:02:36 -0500

    Added more stub functions to the software library, see #2

--- planthub.py
+++ planthub.py
@@ -2,3 +2,69 @@ def info():
     '''Prints a basic library description'''
     print("Software library for the Plant Hub project.")
 
+
+'''Humidity Sensor (DHT11) - Returns two values'''
+def setup_humidity():
+    print("This sets up the humidity sensor")
+
+def read_humidity():
+    print("This reads humidity values from the humidity sensor")
+
+def read_temp():
+    print("This reads temperature values from the humidity sensor")
+
+
+'''Photosensor (DV-P8103)'''
+def setup_photosensor():
+    print("This sets up the photosensor")
+
+def read_light_level():
+    print("This reads the light level from the photosensor")
+
+def output_photosensor():
+    print("This outputs the light level from the photosensor")
+
+
+'''STEMMA Soil Sensor'''
+def setup_soil():
+    print("This sets up the STEMMA Soil Sensor")
+
+def soil_moisture():
+    print("This reads the moisture values from the soil sensor")
+
+def soil_temp():
+    print("This reads the temperature values from the soil sensor")
+
+
+'''LCD Display'''
+def setup_display():
+    print("This sets up the LCD Display")
+
+def turn_on_display():
+    print("This turns on the LCD Display")
+
+def turn_off_display():
+    print("This turns off the LCD Display")
+
+def write_text():
+    print("This writes texts to the LCD Display")
+
+
+'''Potentiometer'''
+def setup_potentiometer():
+    print("This sets up the potentiometer")
+
+def set_resistance_p():
+    print("This sets the resistance for the potentiometer")
+
+
+'''Buttons'''
+def setup_buttons():
+    print("This sets up the buttons")
+
+def detect_push():
+    print("This detects button pushes")
+
+def detect_interrupt():
+    print("This detects interrupts from the buttons")
+
